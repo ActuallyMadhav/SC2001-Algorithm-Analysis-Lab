099ecdb09241a75b9f5bc1d737cd1406ade301a6
ActuallyMadhav <madhavra.2004@gmail.com>
2025-09-24 23:13:25 +0800

    lab2 part(a) completed

--- Lab2/Cpp Solution/lab2Main.cpp
+++ Lab2/Cpp Solution/lab2Main.cpp
@@ -3,15 +3,20 @@
 
 int main(){
     const int INF = INT_MAX;
-    std::vector<std::vector<int>> adjMatrix = {
-        {0, 2, INF, 1},
-        {2, 0, 3, INF},
-        {INF, 3, 0, 4},
-        {1, INF, 4, 0}
+    std::vector<std::vector<int>> graph = {
+        {0, 4, INF, INF, INF, INF, INF, 8, INF},
+        {4, 0, 8, INF, INF, INF, INF, 11, INF},
+        {INF, 8, 0, 7, INF, 4, INF, INF, 2},
+        {INF, INF, 7, 0, 9, 14, INF, INF, INF},
+        {INF, INF, INF, 9, 0, 10, INF, INF, INF},
+        {INF, INF, 4, 14, 10, 0, 2, INF, INF},
+        {INF, INF, INF, INF, INF, 2, 0, 1, 6},
+        {8, 11, INF, INF, INF, INF, 1, 0, 7},
+        {INF, INF, 2, INF, INF, INF, 6, 7, 0}
     };
 
     int source = 0;
 
-    dijkstra_AdjMatrix(adjMatrix, source);
+    dijkstra_AdjMatrix(graph, source);
     return 0;
 }
--- Lab2/Python Solution/dijkstra.py
+++ Lab2/Python Solution/dijkstra.py
@@ -1,2 +1,41 @@
+import sys
+
+def dijkstra_AdjMat(adjMatrix, source):
+    V = len(adjMatrix)
+    dist = [sys.maxsize] * V
+    visited = [False] * V
+
+    dist[source] = 0
+
+    for i in range(V-1):
+        u = -1
+        for j in range(V):
+            if not visited[j] and (u == -1 or dist[j] < dist[u]):
+                u = i
+        
+        visited[u] =  True
+
+        for v in range(V):
+            if not visited[v] and adjMatrix[u][v] != sys.maxsize:
+                dist[v] = min(dist[v], dist[u] + adjMatrix[u][v])
+    
+    for k in range(V):
+        print(f"Distance to {k}: {dist[k]}")
+###########################################################################
+
+if __name__ =="__main__":
+    INF = float('inf')
+    graph = [
+        [0, 4, INF, INF, INF, INF, INF, 8, INF],
+        [4, 0, 8, INF, INF, INF, INF, 11, INF],
+        [INF, 8, 0, 7, INF, 4, INF, INF, 2],
+        [INF, INF, 7, 0, 9, 14, INF, INF, INF],
+        [INF, INF, INF, 9, 0, 10, INF, INF, INF],
+        [INF, INF, 4, 14, 10, 0, 2, INF, INF],
+        [INF, INF, INF, INF, INF, 2, 0, 1, 6],
+        [8, 11, INF, INF, INF, INF, 1, 0, 7],
+        [INF, INF, 2, INF, INF, INF, 6, 7, 0]
+    ]
+    dijkstra_AdjMat(graph, source = 0)
 
 
