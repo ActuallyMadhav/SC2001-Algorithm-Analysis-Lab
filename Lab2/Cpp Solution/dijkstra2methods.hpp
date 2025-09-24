c9e816b8602d3cc533d69a808da0ec23110a5777
ActuallyMadhav <madhavra.2004@gmail.com>
2025-09-24 21:54:23 +0800

    dijkstra init attempt

---
+++ Lab2/Cpp Solution/dijkstra2methods.hpp
@@ -0,0 +1,21 @@
+#ifndef DIJKSTRA2METHODS_HPP
+#define DIJKSTRA2METHODS_HPP
+
+#include <iostream>
+#include <vector>
+#include <limits.h>
+
+// part (a) - using adjacency matrix
+
+void dijkstra_AdjMatrix(const std::vector<std::vector<int>>& adjMatrix, int source){
+
+}
+
+// part (b) - using min heap and adjacency list
+
+void dijkstra_MinHeap(const std::vector<std::vector<std::pair<int, int>>>& adjList, int source){
+
+}
+
+
+#endif
