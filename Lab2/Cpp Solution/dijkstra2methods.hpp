3ec140bab6e73c0378143972fac185c7c39cc9b7
ActuallyMadhav <madhavra.2004@gmail.com>
2025-09-25 13:21:08 +0800

    implemented part(b)

--- Lab2/Cpp Solution/dijkstra2methods.hpp
+++ Lab2/Cpp Solution/dijkstra2methods.hpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <limits.h>
 
 // part (a) - using adjacency matrix
@@ -39,7 +40,42 @@ void dijkstra_AdjMatrix(const std::vector<std::vector<int>>& adjMatrix, int sour
 // part (b) - using min heap and adjacency list
 
 void dijkstra_MinHeap(const std::vector<std::vector<std::pair<int, int>>>& adjList, int source){
+    int V = adjList.size();
+    std::vector<int> dist(V, INT_MAX);
 
+    // this is the min heap
+    std::priority_queue<
+        std::pair<int,int>,     // type of elements stored
+        std::vector<std::pair<int, int>>,       // underlying container (vector)
+        std::greater<>      // comparison function
+    > pq; // name of the priority queue
+
+    dist[source] = 0;
+    pq.push({0, source});
+
+    while(!pq.empty()){
+        int u = pq.top().second;
+        int d_u = pq.top().first;
+        pq.pop();
+
+        if(d_u > dist[u]){
+            continue;
+        }
+
+        //for( auto [v, weight] : adjList[u]){     // this feature is only C++17 and up (structured binding)
+        for(const auto& edge : adjList[u]){        // for C++14 and below, need to declare using .first and .second
+            int v = edge.first;
+            int weight = edge.second;
+            if(dist[u] + weight < dist[v]){
+                dist[v] = dist[u] + weight;
+                pq.push({dist[v], v});
+            }
+        }
+    }
+
+    for(int i = 0; i < V; i++){
+        printf("Distance to %d: %d\n", i, dist[i]);
+    }
 }
 
 
--- Lab2/Cpp Solution/lab2Main.cpp
+++ Lab2/Cpp Solution/lab2Main.cpp
@@ -3,20 +3,30 @@
 
 int main(){
     const int INF = INT_MAX;
-    std::vector<std::vector<int>> graph = {
-        {0, 4, INF, INF, INF, INF, INF, 8, INF},
-        {4, 0, 8, INF, INF, INF, INF, 11, INF},
-        {INF, 8, 0, 7, INF, 4, INF, INF, 2},
-        {INF, INF, 7, 0, 9, 14, INF, INF, INF},
-        {INF, INF, INF, 9, 0, 10, INF, INF, INF},
-        {INF, INF, 4, 14, 10, 0, 2, INF, INF},
-        {INF, INF, INF, INF, INF, 2, 0, 1, 6},
-        {8, 11, INF, INF, INF, INF, 1, 0, 7},
-        {INF, INF, 2, INF, INF, INF, 6, 7, 0}
+
+    //////////////// PART (A) ////////////////////////
+    std::vector<std::vector<int>> adjMatrix = {
+        {0, 2, INF, 1},
+        {2, 0, 3, INF},
+        {INF, 3, 0, 4},
+        {1, INF, 4, 0}
     };
 
-    int source = 0;
+    int source1 = 0;
 
-    dijkstra_AdjMatrix(graph, source);
+    dijkstra_AdjMatrix(adjMatrix, source1);
+
+    //////////////// PART (B) ////////////////////////
+    std::vector<std::vector<std::pair<int, int>>> adjList = {
+        {{1, 2}, {3, 1}},     // Node 0
+        {{0, 2}, {2, 3}},     // Node 1
+        {{1, 3}, {3, 4}},     // Node 2
+        {{0, 1}, {2, 4}}      // Node 3
+    };
+
+    int source2 = 0;
+
+    dijkstra_MinHeap(adjList, source2);
+
     return 0;
 }
